28833b4fc1628700f352933e7440d63efb704f20
Montassar Hajri <montassarhajri@outlook.com>
2024-07-20 02:55:12 +0100

    task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -13,38 +13,32 @@ an expiration time of 10 seconds."""
 
 import redis
 import requests
+from typing import Callable
 from functools import wraps
 
-r = redis.Redis()
+rd = redis.Redis()
 
 
-def url_access_count(method):
-    """decorator for get_page function"""
+def count_requests(method: Callable) -> Callable:
+    """requests"""
+
     @wraps(method)
     def wrapper(url):
-        """wrapper function"""
-        key = "cached:" + url
-        cached_value = r.get(key)
-        if cached_value:
-            return cached_value.decode("utf-8")
+        """wrapper"""
+        rd.incr(f"count:{url}")
+        cached_html = rd.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
 
-            # Get new content and update cache
-        key_count = "count:" + url
-        html_content = method(url)
+        html = method(url)
+        rd.setex(f"cached:{url}", 10, html)
+        return html
 
-        r.incr(key_count)
-        r.set(key, html_content, ex=10)
-        r.expire(key, 10)
-        return html_content
     return wrapper
 
 
-@url_access_count
+@count_requests
 def get_page(url: str) -> str:
-    """obtain the HTML content of a particular"""
-    results = requests.get(url)
-    return results.text
-
-
-if __name__ == "__main__":
-    get_page('https://httpbin.org/delay/10')
+    """pages"""
+    req = requests.get(url)
+    return req.text
